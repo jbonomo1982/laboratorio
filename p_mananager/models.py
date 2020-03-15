9d2f9a4a1d95607eaa9c6a512018f1e6f5c99fba
juan <j.m.bonomo1982@gmail.com>
2020-03-15 15:35:46 -0300

    Cambio de modelos en p_m

--- p_mananager/models.py
+++ p_mananager/models.py
@@ -12,7 +12,23 @@ class Proyectos(models.Model):
     publico = models.BooleanField(default= True,help_text="Indica si el proyecto puede ser visto por todos, o no.")
     nombre_proyecto = models.CharField(help_text='nombre del proyecto',max_length=50)
     descripcion = models.TextField(help_text='descripción del proyecto')
+    terminado = models.BooleanField(default= False,help_text="Indica si el proyecto está terminado")
 
     def __str__(self):
         d = self.nombre_proyecto
+        return d
+
+class Tareas(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='autor') #El que crea el proyecto.
+    proyecto = models.ForeignKey(Proyectos, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_comienzo = models.DateField(help_text="fecha en que comienza la tarea", blank=True, null=True)
+    fecha_final = models.DateField(help_text="fecha en que termina la tarea", blank=True, null=True)
+    publico = models.BooleanField(default= True,help_text="Indica si el proyecto puede ser visto por todos, o no.")
+    nombre_tarea = models.CharField(help_text='nombre del proyecto',max_length=50)
+    descripcion = models.TextField(help_text='descripción del proyecto')
+    terminado = models.BooleanField(default= False,help_text="Indica si el proyecto está terminado")
+
+    def __str__(self):
+        d = self.nombre_tarea
         return d
